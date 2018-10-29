9c25dad6026843c50befd0d078e6d88a0f853267
Trista <white1992v@gmail.com>
2018-10-29 10:50:33 +0800

    landingpage 轉址到 plan

--- src/js/components/landingpage.js
+++ src/js/components/landingpage.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { NavLink } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
-const LandingPage = () => {
-    return (
-        <div>
-            LandingPage
-            <NavLink to="/plan">plan</NavLink>
-            <NavLink to="/profile">planprofile</NavLink>
-        </div>
-    );
-};
-
+class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <Redirect to="/plan" />;
+    }
+}
 export default LandingPage;
